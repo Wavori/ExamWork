6b412137064b4117027660257c69de79835012a2
Wavori <koreev1001@gmail.com>
2024-10-15 00:02:11 +0300

    ExamWork

---
+++ TestWork/FragrantWorld/FragrantWorld/Windows/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace FragrantWorld.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainFrame_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+
+        }
+    }
+}
